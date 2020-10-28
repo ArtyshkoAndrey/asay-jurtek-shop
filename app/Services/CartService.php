18bc23db87bb39c5d3fc328e276fece72e02c6c5
Fulliton <artyshko.andrey@gmail.com>
2020-10-28 21:33:36 +0700

    Update CartService.php

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -2,119 +2,65 @@
 
 namespace App\Services;
 
-use App\Models\Product;
-use Auth;
 use App\Models\CartItem;
+use App\Exceptions\InvalidRequestException;
+use Illuminate\Support\Facades\Auth;
 
-class CartService
-{
+class CartService {
   public function get()
   {
-    $cartItems = Auth::user()->cartItems()->with(['productSku.product'])->get();
-    $ids = [];
-    foreach ($cartItems as $item) {
-      $ids += array_fill(count($ids), $item->amount, $item->product_sku_id);
-    }
-    $productsSku = Product::getProducts($ids);
+    $cartItems = Auth::user()->cartItems()->with(['product'])->get();
     $priceAmount = 0;
-    $cartItems = [];
-    $amount = 0;
-    foreach ($productsSku as $k => $productSku) {
-      $id = (int)$productSku->id;
-      $amount++;
-      $ch = false;
-      $item = [];
-
-      foreach ($cartItems as $key => $item) {
-        if (($item['id'] === $productSku->id) && ($item['product_sku']->product->price === $productSku->product->price)) {
-          $ch = true;
-          $cartItems[$key]['amount'] = $item['amount'] + 1;
-          $priceAmount += $productSku->product->on_sale ? $productSku->product->price_sale : $productSku->product->price;
-          break;
-        }
-      }
-      if (!$ch) {
-        $item['amount'] = 1;
-        $item['id'] = $id;
-        $item['product_sku'] = $productSku;
-        $priceAmount += $productSku->product->on_sale ? $productSku->product->price_sale : $productSku->product->price;
-        array_push($cartItems, $item);
-      }
+    $items = [];
+    foreach ($cartItems as $cartItem) {
+      $product = $cartItem->product;
+      array_push($items, $product);
+      $priceAmount += $product->on_sale ? $product->price_sale : $product->price;
     }
-    return ['amount'=> $amount, 'priceAmount' => $priceAmount, 'cartItems' => $cartItems];
+    $amount = count($cartItems);
+    return ['amount'=> $amount, 'priceAmount' => $priceAmount, 'cartItems' => $items];
   }
 
-  public function add($skuId, $amount)
+  public function add($id)
   {
     $user = Auth::user();
-    // 从数据库中查询该商品是否已经在购物车中
-    if ($item = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-      // 如果存在则直接叠加商品数量
-      $item->update([
-        'amount' => $item->amount + $amount,
-      ]);
+    if ($item = $user->cartItems()->where('product_id', $id)->first()) {
+      throw new InvalidRequestException('Товар уже в корзине');
     } else {
-      // 否则创建一个新的购物车记录
-      $item = new CartItem(['amount' => $amount]);
+      $item = new CartItem();
+      $item->product()->associate($id);
       $item->user()->associate($user);
-      $item->productSku()->associate($skuId);
       $item->save();
     }
-
     return $item;
   }
 
-  public function minusAmount($skuId, $amount)
-  {
-    $user = Auth::user();
-    if ($item = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-      if ($amount <= 0) {
-        $item->update([
-          'amount' => $item->amount - 1
-        ]);
-      } else {
-        $item->update([
-          'amount' => $amount,
-        ]);
-      }
-    } else {
-      // 否则创建一个新的购物车记录
-      $item = new CartItem(['amount' => $amount]);
-      $item->user()->associate($user);
-      $item->productSku()->associate($skuId);
-      $item->save();
-    }
 
-    return $item;
+  public function remove($ids)
+  {
+    if (!is_array($ids)) {
+        $ids = [$ids];
+    }
+    Auth::user()->cartItems()->whereIn('product_id', $ids)->delete();
   }
 
-  public function remove($skuIds)
+  public function deleteAll()
   {
-      // 可以传单个 ID，也可以传 ID 数组
-      if (!is_array($skuIds)) {
-          $skuIds = [$skuIds];
-      }
-      Auth::user()->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+    dd(Auth::user()->cartItems()->get());
+    Auth::user()->cartItems()->all()->delete();
   }
 
   public function amount ()
   {
     $user = Auth::user();
-    return $user->cartItems()->get()->map(function ($item) {
-      return $item->amount;
-    })->sum();
+    return count($user->cartItems()->get());
   }
 
   public function priceAmount ()
   {
     $user = Auth::user();
-    $user->cartItems()->get()->map(function ($item) {
-      if($item->productSku->product === null) {
-        $item->delete();
-      }
-    });
     return $user->cartItems()->get()->map(function ($item) {
-      return ($item->productSku->product->on_sale ? $item->productSku->product->price_sale : $item->productSku->product->price) * $item->amount;
+      return $item->product->on_sale ? $item->product->price_sale : $item->product->price;
     })->sum();
   }
 }
