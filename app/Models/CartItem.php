788e592502c744dca12e65907db97c81df96dccc
Fulliton <artyshko.andrey@gmail.com>
2020-10-28 21:32:24 +0700

    Связь с продуктом в Итемах

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
   use HasFactory;
-  protected $fillable = ['amount'];
+  protected $fillable = ['product_id'];
   public $timestamps  = false;
 
   public function user()
   {
     return $this->belongsTo(User::class);
   }
+
+  public function product()
+  {
+    return $this->belongsTo(Product::class);
+  }
+
 }
