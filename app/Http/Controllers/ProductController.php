0cb1d1f971bc905eeccd4d966b03265b726af017
Fulliton <artyshko.andrey@gmail.com>
2020-10-28 21:33:32 +0700

    Удаление продуктор и добавление

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CartItem;
 use App\Models\Product;
+use App\Services\CartService;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller {
 
-  public function show ($id) {
+  protected $cartService;
+
+  public function __construct(CartService $cartService)
+  {
+    parent::__construct($cartService);
+  }
+  public function show ($id, Request $request) {
     $item = Product::with('photos')->find($id);
+    $inCart = false;
+    if ($user = $request->user()) {
+      $inCart = (bool) $user->cartItems()->where('product_id', $id)->first();
+    }
     $items = Product::take(4)->get(); // TODO: Сделать вывод из той же категории
-    return view('item', compact('item', 'items'));
+    return view('item', compact('item', 'items', 'inCart'));
+  }
+
+  public function addCart ($id) {
+    if(Auth::check()) {
+      $this->cartService->add($id);
+      return redirect()->back();
+    }
+  }
+
+  public function removeCart ($id) {
+    if(Auth::check()) {
+      $this->cartService->remove($id);
+      return redirect()->back();
+    }
+  }
+
+  public function removeAllCart () {
+    dd(123);
+    if(Auth::check()) {
+      $this->cartService->deleteAll();
+
+      return redirect()->back();
+    }
   }
 }
