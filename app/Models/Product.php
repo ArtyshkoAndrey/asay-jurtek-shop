69b459b067db5c771097ee22da6e876e961a8f6c
Fulliton <artyshko.andrey@gmail.com>
2020-11-03 18:06:36 +0700

    Документация функций в моделях

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static take(int $int)
+ */
 class Product extends Model
 {
   use HasFactory;
