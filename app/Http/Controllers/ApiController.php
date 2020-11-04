903bb7d59089cf0be4bc7834cfbd18b22f9ddc8e
Fulliton <artyshko.andrey@gmail.com>
2020-11-04 20:30:01 +0700

    Компании в api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,22 +8,47 @@ use App\Models\Category;
 use App\Models\City;
 use App\Models\Country;
 use App\Models\Currency;
+use App\Models\ExpressCompany;
+use App\Models\ExpressZone;
 use App\Models\Product;
+use App\Services\CartService;
+use http\Env\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class ApiController
+ * @package App\Http\Controllers
+ */
 class ApiController extends Controller
 {
-  public function __constructor()
+  public function __construct(CartService $cartService)
   {
-    //
+    parent::__construct($cartService);
   }
 
-  public function getCurrency ($id) {
+
+  /**
+   * function for get currency by id
+   *
+   * @param int $id
+   * @return JsonResponse
+   */
+  public function getCurrency (int $id): JsonResponse
+  {
     $currency = Currency::find($id);
     return response()->json(['currency' => $currency]);
   }
 
-  public function productsCheck (Request $request) {
+  /**
+   * Check actual items by ids
+   *
+   * @param Request $request
+   * @return JsonResponse
+   */
+  public function productsCheck (Request $request): JsonResponse
+  {
     $request->validate([
       'ids' => 'required|array',
       'ids.*' => 'integer'
@@ -31,29 +56,96 @@ class ApiController extends Controller
     $items = Product::findMany($request->ids);
     return response()->json(['items' => $items]);
   }
-  public function city ($city, $country = null) {
+
+  /**
+   * get cities by filter name or get cities in country
+   *
+   * @param string $city
+   * @param int|null $country
+   * @return JsonResponse
+   */
+  public function city (string $city, int $country = null): JsonResponse
+  {
     if ($country === null)
       $cities = City::whereLike('name', $city)->take(20)->get();
     else
       $cities = City::whereHas('country', function ($q) use($country) {
         $q->where('countries.id', $country);
       })->whereLike('name', $city)->take(20)->get();
-    return ['items' => $cities];
+    return response()->json(['items' => $cities]);
   }
 
-  public function country ($country) {
+  /**
+   * get countries by filter name
+   *
+   * @param string $country
+   * @return JsonResponse
+   */
+  public function country (string $country): JsonResponse
+  {
     $countries = Country::whereLike('name', $country)->get();
-    return ['items' => $countries];
+    return response()->json(['items' => $countries]);
   }
 
-  public function category ($category) {
+  /**
+   * get categories by filter name
+   *
+   * @param string $category
+   * @return JsonResponse
+   */
+  public function category (string $category): JsonResponse
+  {
     $categories = Category::whereLike('name', $category)->get();
-    return ['items' => $categories];
+    return response()->json(['items' => $categories]);
   }
 
-  public function brand ($brand) {
+  /**
+   * get brands by filters name
+   *
+   * @param string $brand
+   * @return JsonResponse
+   */
+  public function brand (string $brand): JsonResponse
+  {
     $brands = Brand::whereLike('name', $brand)->get();
-    return ['items' => $brands];
+    return response()->json(['items' => $brands]);
+  }
+
+  /**
+   * Function for filtered Express companies
+   *
+   * @param Request $request
+   * @return array
+   */
+  public function companies(Request $request): array
+  {
+    $express_companies = ExpressCompany::where('name', '!=', 'Самовывоз')->get();
+    $zones = ExpressZone::with('company')->whereHas('cities', function ($qq) use ($request) {
+      $qq->where('cities.id', $request->city);
+    })->get();
+    $express_companies = $express_companies->toArray();
+    for($i=0;$i<count($express_companies); $i++) {
+      foreach ($zones as $z) {
+        if($z->company->id === $express_companies[$i]['id']) {
+          if ($z->step_cost_array !== null) {
+            $express_companies[$i]['costedTransfer'] = $z->step_cost_array;
+            $express_companies[$i]['step_unlim'] = null;
+            $express_companies[$i]['step_cost_unlim'] = null;
+          } else {
+            $express_companies[$i]['costedTransfer'] = $z->cost;
+            $express_companies[$i]['step_unlim'] = $z->step;
+            $express_companies[$i]['step_cost_unlim'] = $z->cost_step;
+          }
+        }
+      }
+      if (!isset($express_companies[$i]['costedTransfer'])) {
+        $express_companies[$i]['costedTransfer'] = null;
+        $express_companies[$i]['costedTransfer'] = null;
+        $express_companies[$i]['step_unlim'] = null;
+        $express_companies[$i]['step_cost_unlim'] = null;
+      }
+    }
+    return $express_companies;
   }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
-//Route::get('/test', function () {
-//  (new App\Models\Setting)->setHeader();
-//});
-if ((new App\Models\Setting)->statusSite()) {
-  Auth::routes();
 
+if ((new App\Models\Setting)->statusSite()) {
   Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
   Route::get('/contacts', [App\Http\Controllers\HomeController::class, 'contacts'])->name('contacts');
   Route::post('/currency/change', [App\Http\Controllers\CurrencyController::class, 'change'])->name('currency.change');
@@ -17,50 +13,45 @@ if ((new App\Models\Setting)->statusSite()) {
     Route::post('/{id}', [\App\Http\Controllers\ProductController::class, 'addCart'])->name('addCart');
     Route::delete('/{id}', [\App\Http\Controllers\ProductController::class, 'removeCart'])->name('removeCart');
   });
-
-    getAdminRoute();
- } else {
-  Auth::routes();
-  getAdminRoute();
- }
-
-
-function getAdminRoute() {
-  Route::middleware(['admin'])->prefix('admin')->name('admin.')->group(function () {
-
-    Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('root');
-    Route::put('status', [\App\Http\Controllers\Admin\DashboardController::class, 'status'])->name('root.status');
-
-    Route::name('store.')->group(function () {
-
-      Route::resource('order', App\Http\Controllers\Admin\OrderController::class);
-      Route::delete('/order/all', [App\Http\Controllers\Admin\OrderController::class,'collectionsDestroy'])->name('order.collectionsDestroy');
-
-      Route::resource('express', App\Http\Controllers\Admin\ExpressController::class);
-      Route::put('/express/enabled/{id}', [App\Http\Controllers\Admin\ExpressController::class, 'enabled'])->name('express.enabled');
-
-      Route::resource('express-zone', App\Http\Controllers\Admin\ExpressZoneController::class);
-      Route::post('/express-zone/{id}/destroy', [App\Http\Controllers\Admin\ExpressZoneController::class, 'destroyCity'])->name('express-zone.destroyCity');
-
-      Route::resource('pay', App\Http\Controllers\Admin\PayController::class);
-
-
-
-    });
-
-    Route::name('production.')->group(function () {
-      Route::name('products.')->prefix('products')->group(function () {
-        Route::delete('/all', [App\Http\Controllers\Admin\ProductsController::class, 'collectionsDestroy'])->name('collectionsDestroy');
-        Route::post('/all', [App\Http\Controllers\Admin\ProductsController::class, 'collectionsRestore'])->name('collectionsRestore');
-        Route::post('/{id}/photo', [App\Http\Controllers\Admin\ProductsController::class, 'photo'])->name('photo');
-        Route::post('/photo-create', [App\Http\Controllers\Admin\ProductsController::class, 'photoCreate'])->name('photoCreate');
-        Route::post('/{id}/photo-delete', [App\Http\Controllers\Admin\ProductsController::class, 'photoDelete'])->name('photoDelete');
-        Route::post('/photo-delete-create', [App\Http\Controllers\Admin\ProductsController::class, 'photoDeleteCreate'])->name('photoDeleteCreate');
-
-      });
-      Route::resource('/products', App\Http\Controllers\Admin\ProductsController::class);
-      Route::resource('/attr', App\Http\Controllers\Admin\SkusController::class);
-      Route::resource('/skus-category', App\Http\Controllers\Admin\SkusCategoriesController::class);
-    });
-  });
+  Route::resource('/order', \App\Http\Controllers\OrderController::class);
 }
+
+Auth::routes();
+
+Route::middleware(['admin'])->prefix('admin')->name('admin.')->group(function () {
+
+  Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('root');
+  Route::put('status', [\App\Http\Controllers\Admin\DashboardController::class, 'status'])->name('root.status');
+
+  Route::name('store.')->group(function () {
+
+    Route::resource('order', App\Http\Controllers\Admin\OrderController::class);
+    Route::delete('/order/all', [App\Http\Controllers\Admin\OrderController::class,'collectionsDestroy'])->name('order.collectionsDestroy');
+
+    Route::resource('express', App\Http\Controllers\Admin\ExpressController::class);
+    Route::put('/express/enabled/{id}', [App\Http\Controllers\Admin\ExpressController::class, 'enabled'])->name('express.enabled');
+
+    Route::resource('express-zone', App\Http\Controllers\Admin\ExpressZoneController::class);
+    Route::post('/express-zone/{id}/destroy', [App\Http\Controllers\Admin\ExpressZoneController::class, 'destroyCity'])->name('express-zone.destroyCity');
+
+    Route::resource('pay', App\Http\Controllers\Admin\PayController::class);
+
+
+
+  });
+
+  Route::name('production.')->group(function () {
+    Route::name('products.')->prefix('products')->group(function () {
+      Route::delete('/all', [App\Http\Controllers\Admin\ProductsController::class, 'collectionsDestroy'])->name('collectionsDestroy');
+      Route::post('/all', [App\Http\Controllers\Admin\ProductsController::class, 'collectionsRestore'])->name('collectionsRestore');
+      Route::post('/{id}/photo', [App\Http\Controllers\Admin\ProductsController::class, 'photo'])->name('photo');
+      Route::post('/photo-create', [App\Http\Controllers\Admin\ProductsController::class, 'photoCreate'])->name('photoCreate');
+      Route::post('/{id}/photo-delete', [App\Http\Controllers\Admin\ProductsController::class, 'photoDelete'])->name('photoDelete');
+      Route::post('/photo-delete-create', [App\Http\Controllers\Admin\ProductsController::class, 'photoDeleteCreate'])->name('photoDeleteCreate');
+
+    });
+    Route::resource('/products', App\Http\Controllers\Admin\ProductsController::class);
+    Route::resource('/attr', App\Http\Controllers\Admin\SkusController::class);
+    Route::resource('/skus-category', App\Http\Controllers\Admin\SkusCategoriesController::class);
+  });
+});
