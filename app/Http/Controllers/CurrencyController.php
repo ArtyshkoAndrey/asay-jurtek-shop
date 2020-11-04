245cd69e551f24276e22c53d3c673eca8a307baf
Artyshko Andrey <artyshko.andrey@gmail.com>
2020-11-04 23:08:29 +0700

    phpdoc + TODO CurrencyController

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -8,11 +8,26 @@ use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
+
+  // TODO: Ретурн тип изменить
+
+  /**
+   * Override contructor
+   * @return type
+   */
   public function __constructor()
   {
     //
   }
 
+  // TODO: Ретурн тип изменить, в функции изменить тип
+
+  /**
+   * function for change currency User
+   * 
+   * @param Request $request 
+   * @return Response
+   */
   public function change(Request $request) {
     Helpers::changeCurrency($request->currency);
     return redirect()->back();
