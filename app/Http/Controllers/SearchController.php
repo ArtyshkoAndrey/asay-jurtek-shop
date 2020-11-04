a34bb4ecfbbef9b0a79767f49376af4731de4c79
Artyshko Andrey <artyshko.andrey@gmail.com>
2020-11-04 23:18:53 +0700

    phpdoc + TODO SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
-
 class SearchController extends Controller {
 
+  // TODO: Изменить ретурн phpdov
+
+  /**
+   * Filter items and display search page
+   * @param Request $request 
+   * @return type
+   */
   public function index (Request $request) {
     $q = $request->q;
     $items = Product::where('title', 'LIKE', '%' . $q . '%')->get();
