2bf22f0fb3593156fc8ddbd7b64691eb86a2a598
Artyshko Andrey <artyshko.andrey@gmail.com>
2020-11-04 23:02:53 +0700

    Один this + phpdoc Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,19 +14,26 @@ class Controller extends BaseController
   use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
   protected $cartService;
 
+  /**
+   * Contructor, shared vars in views
+   * 
+   * @param CartService $cartService 
+   * @return type
+   */
   public function __construct (CartService $cartService) {
-    $this->cartService = $cartService;
-    $this->middleware(function ($request, $next) {
-      $currency = Helpers::currency();
-      $cartItems = [];
-      $amount = 0;
-      $priceAmount = 0;
-      auth()->user() ? extract($this->cartService->get(), EXTR_REFS) : null;
-      view()->share('currency', $currency);
-      view()->share('cartItems', $cartItems);
-      view()->share('priceAmount', number_format($priceAmount * $currency->ratio, null, null, ' '));
-      view()->share('amount', $amount);
-      return $next($request);
-    });
+    $this
+      ->cartService = $cartService;
+      ->middleware(function ($request, $next) {
+        $currency = Helpers::currency();
+        $cartItems = [];
+        $amount = 0;
+        $priceAmount = 0;
+        auth()->user() ? extract($this->cartService->get(), EXTR_REFS) : null;
+        view()->share('currency', $currency);
+        view()->share('cartItems', $cartItems);
+        view()->share('priceAmount', number_format($priceAmount * $currency->ratio, null, null, ' '));
+        view()->share('amount', $amount);
+        return $next($request);
+      });
   }
 }
