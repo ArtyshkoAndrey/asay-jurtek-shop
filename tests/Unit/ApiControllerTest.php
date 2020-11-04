ccf5b47a9ebe06d76b2d3113231956a05a95506d
Fulliton <artyshko.andrey@gmail.com>
2020-11-04 20:33:19 +0700

    Tests

---
+++ tests/Unit/ApiControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ApiControllerTest extends TestCase
+{
+
+//  TODO: После выполнения проекта написать тесты.
+  public function testGetCurrency()
+  {
+
+    $response = $this->json('POST', 'api/currency/get/1');
+
+    $response
+      ->assertStatus(200)
+      ->assertJson([
+        'created_at' => true,
+      ]);
+  }
+
+  public function testProductsCheck()
+  {
+
+  }
+
+  public function testCountry()
+  {
+
+  }
+
+  public function testCategory()
+  {
+
+  }
+
+  public function testBrand()
+  {
+
+  }
+
+  public function testCity()
+  {
+
+  }
+}
