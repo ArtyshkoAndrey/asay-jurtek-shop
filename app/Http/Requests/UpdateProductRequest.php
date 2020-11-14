c3bd0485ac06d7f2abb1af6910b92c738bcf5e8b
Fulliton <artyshko.andrey@gmail.com>
2020-11-14 22:14:32 +0700

    Валидаторы для товара

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'title'       => 'required|string|max:255',
+      'description' => 'required',
+      'price'       => 'required|integer|min:0',
+      'price_sale'  => 'sometimes|integer|min:0|nullable',
+      'weight'      => 'required|min:0',
+      'on_new'      => 'boolean',
+      'on_sale'     => 'boolean',
+      'meta'        => 'required|array',
+      'sex'         => 'required|string',
+      'status'      => 'required|string',
+      'category'    => 'required|exists:App\Models\Category,id',
+      'skus'        => 'required|exists:App\Models\Skus,id'
+    ];
+  }
+}
