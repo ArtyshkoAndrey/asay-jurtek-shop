05ea4105eaec2b67572479030dae92d8cd5e8a02
Fulliton <artyshko.andrey@gmail.com>
2020-11-27 20:24:40 +0700

    Фикс каортинок в новостях

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -4,25 +4,32 @@
 @section('style')
   <style>
     #header-image {
-      background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("{{ asset('storage/news/'.$news->image) }}");
+      background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("{{ asset('storage/news/'.$news->image) }}") no-repeat center;
       background-size: cover;
-      background-repeat: no-repeat;
-      background-position: center;
       padding: 200px 0;
+    }
+
+    #news img {
+      width: 100%;
+      height: auto;
     }
   </style>
 @endsection
 
 @section('content')
 
-  <div class="container-fluid mt-4">
+  <div class="container-fluid mt-4 px-0">
     <div class="row ml-0">
-      <div class="col-12" id="header-image">
-        <h1 class="h2 text-center text-white">{{ $news->title }}</h1>
+      <div class="col-12 px-0 m-0" id="header-image">
+        <div class="row justify-content-center">
+          <div class="col-10">
+            <h1 class="h2 text-center text-white">{{ $news->title }}</h1>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row mt-5">
-      <div class="col-10 offset-1">
+      <div class="col-10 offset-1" id="news">
         {!! $news->content !!}
       </div>
     </div>
