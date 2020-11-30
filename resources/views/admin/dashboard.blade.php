0077398ff536b3c380c2756ca78c63639491d320
Fulliton <artyshko.andrey@gmail.com>
2020-12-01 00:34:08 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,8 +5,8 @@
   <div class="container-fluid px-4">
     <div class="row">
       <div class="col-lg-4 col-md-6 col-12 mt-5">
-        <div class="card h-100 border-0 w-100 rounded-0" style="z-index: 90;box-shadow: 0 18px 19px rgba(0, 0, 0, 0.25)">
-          <div class="card-header">
+        <div class="card border-0 w-100 rounded-0" style="z-index: 90;box-shadow: 0 18px 19px rgba(0, 0, 0, 0.25)">
+          <div class="card-header font-weight-bolder">
             Работа сайта
           </div>
           <div class="card-body">
@@ -25,6 +25,30 @@
                 </div>
               </div>
             </form>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-12 mt-5">
+        <div class="card border-0 w-100 rounded-0" style="z-index: 90;box-shadow: 0 18px 19px rgba(0, 0, 0, 0.25)">
+          <div class="card-header font-weight-bolder">
+            Инфорамация
+          </div>
+          <div class="card-body">
+            <p>Административная панель разработана программистом <a href="{{ url('https://www.vk.com/fulliton') }}" class="text-red" target="_blank">Fulliton</a></p>
+            <p>Возможны модификации и добавления функционала:</p>
+            <ul>
+              <li>Yandex метрика</li>
+              <li>Telegram Bot</li>
+              <li>VK Bot</li>
+              <li>1 и более размеров товара</li>
+              <li>Подробные графики учёта</li>
+              <li>Тёмная тема</li>
+              <li>Добавление страниц на сайте</li>
+              <li>Рассылка сообщений на почту пользователям</li>
+              <li>Добавление методов оплаты</li>
+              <li>Добавление валют</li>
+            </ul>
           </div>
         </div>
       </div>
